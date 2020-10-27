24476d0ff800e03d6166a464586fe97e8c4154f4
Lucas Fernandes <scoutontherocks@gmail.com>
2020-10-26 23:20:45 -0400

    initial site redesign work

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("storesapp", ["ngRoute"]);
-app.config(function ($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(false);
-  $routeProvider
-    .when("/index", {
-      templateUrl: "views/map.html",
-    })
-    .otherwise("/index");
-});
