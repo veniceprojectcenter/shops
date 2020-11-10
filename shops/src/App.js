499e543605c25dd0c2f52f616cfcb4cfc6f4738b
Lucas Fernandes <46578395+lafernandes22@users.noreply.github.com>
2020-11-09 22:57:39 -0500

    starting map filter component

--- shops/src/App.js
+++ shops/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MapView from './components/MapView';
+import MapFilter from './components/MapFilter';
 import './css/homeMap.css';
 import './css/siteLayout.css';
 
@@ -8,12 +9,12 @@ function App() {
     <div className="content">
       <div className="site-header">
         {/* Hamburger Menu */}
-        <h1 id="page-title">Shops by Venice Project Center</h1>
+        <h1 id="page-title">Venice Shops</h1>
+        {/* <a href="http://www.veniceprojectcenter.org/vpc"><h1 id="VPC">Visit VPC</h1></a> */}
       </div>
       <div className="site-body">
-        <div className= "map">
         <MapView />
-        </div>
+        <MapFilter />
       </div>
     </div>
   );
---
+++ shops/src/components/MapFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MapFilter extends React.Component{
+    render(){
+        return(
+            <div id="mapFilter">
+                <h1> Filters</h1>
+            </div>
+        );
+    }
+}
+export default MapFilter;
