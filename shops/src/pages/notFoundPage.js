551ec879dbe13fc30fdb917de17cc1b03581ca49
Lucas Fernandes <scoutontherocks@gmail.com>
2020-11-16 01:20:23 -0500

    finished 404 page

--- shops/src/pages/notFoundPage.js
+++ shops/src/pages/notFoundPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import '../css/404.css';
 
 function NotFoundPage() {
     return(
-        <div>
-            <h1>404</h1>
+        <div class="site-body">
+            <h1 id="404">404</h1>
+            <h2 id="404"> Page not found, please either redirect yourself with a new request or use the navigation menu above.</h2>
         </div>
     );
 }
