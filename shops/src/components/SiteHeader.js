24d80e0a80732d449794aa47e43fec5178d7276d
Lucas Fernandes <46578395+lafernandes22@users.noreply.github.com>
2020-11-16 22:32:01 -0500

    more styling fixes

--- shops/src/components/SiteHeader.js
+++ shops/src/components/SiteHeader.js
@@ -5,13 +5,11 @@ import '../css/sideBar.css';
 
 
 class SiteHeader extends React.Component{
-    
-
     render(){
         return(
             <div className="site-header">
                 <Sidebar pageWrapID={'site-body'} outerContainerId={'site-header'} />
-                <h1 id="page-title">VENICE SHOPS</h1>
+                <h1 id="site-title">VENICE SHOPS</h1>
             </div>
         );
     }
